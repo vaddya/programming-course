e5087ff8a6515d716979fc6807765c9a9f60dfd4
Vadim Dyachkov <vadik.dyachkov@gmail.com>
2016-02-14 02:11:44 +0300

    Modified doxygen cfg

--- sources/homework/cpplib/circlegame.h
+++ sources/homework/cpplib/circlegame.h
@@ -1,23 +1,42 @@
 #ifndef CIRCLEGAME_H
 #define CIRCLEGAME_H
 
-/* #3: CIRCLE GAME
- * По кругу располагаются NUM_OF_PLAYERS человек. Ведущий считает по кругу, начиная с первого, и выводит NUM_FOR_KICK-го человека.
- * Круг смыкается, счет возобновляется со следующего; так  продолжается, пока в круге не останется только один человек. Найти номер этого человека.
- */
-
 #include <vector>
 using std::vector;
+/**
+  @brief Круговая игра
 
+  По кругу располагаются num_of_players человек. Ведущий считает по кругу, начиная с первого, и выводит num_for_kick-го человека.
+  Круг смыкается, счет возобновляется со следующего. Так  продолжается, пока в круге не останется только один человек. Найти номер этого человека.
+  */
 class CircleGame
 {
 private:
     int players;
     int kicking;
 public:
-    CircleGame(const int, const int);
-    void setPlayers(const int);
-    void setKicking(const int);
+    /**
+     * @brief Конструктор
+     * @param num_of_players количество игроков, учавствующих в игре
+     * @param num_for_kick номер человека, которого выводят из игры
+     */
+    CircleGame(const int num_of_players, const int num_for_kick);
+    /**
+     * @brief Установить значение поля players, равное количеству игроков
+     * @param num_of_players новое количество игроков, учавствующих в игре
+     */
+    void setPlayers(const int num_of_players);
+    /**
+     * @brief Установить значение поля kicking, равное номеру человека, которого выводят
+     * @param num_for_kick новый номер человека, которого выводят из игры
+     */
+    void setKicking(const int num_for_kick);
+    /**
+     * @brief Определить победителя
+     *
+     * Данная функция высчитывает номер игрока-победителя. Ожидается что значения players и kicking известны.
+     * @return Номер победившего игрока
+     */
     int determineWinner() const;
 };
 
--- sources/homework/cpplib/meter.h
+++ sources/homework/cpplib/meter.h
@@ -1,9 +1,10 @@
 #ifndef METER_H
 #define METER_H
-/* #1.1: INCH TO CM
- * Перевести длину отрезка из дюймов в метры, сантиметры и миллиметры.
- */
+/**
+  @brief Перевод из дюймов в метры.
 
+  Перевести длину отрезка из дюймов в метры, сантиметры и миллиметры.
+*/
 class Meter
 {
 private:
@@ -13,11 +14,34 @@ private:
     double mm;
     void convertInchToCm();
 public:
+    /**
+      @brief Конструктор
+    */
     Meter();
-    void fromInches(const int);
+    /**
+     * @brief Перевести из дюймов в метрическую систему
+     * @param inches число дюймов
+     */
+    void fromInches(const int inches);
+    /**
+     * @brief Получить число дюймов
+     * @return число дюймов
+     */
     int getInches() const { return inches; }
+    /**
+     * @brief Получить количество метров
+     * @return количество метров
+     */
     int getM() const { return m; }
+    /**
+     * @brief Получить количество сантиметров
+     * @return количество сантиметров
+     */
     int getCm() const { return cm; }
+    /**
+     * @brief Получить количество миллиметров
+     * @return количество миллиметров
+     */
     double getMm() const { return mm; }
 };
 
